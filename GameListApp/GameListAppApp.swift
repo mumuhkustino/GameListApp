23b4bfa46cdb363fbd003baeb3d3dbf7f0d300fb
Mumuh <mumuhandev@gmail.com>
2022-01-09 16:02:31 +0700

    Initial Commit

---
+++ GameListApp/GameListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameListAppApp.swift
+//  GameListApp
+//
+//  Created by C-70 on 09/01/22.
+//
+
+import SwiftUI
+
+@main
+struct GameListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
