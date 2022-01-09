d0fb4373774381681a5a00098e351534ff0388e4
Mumuh <mumuhandev@gmail.com>
2022-01-09 22:40:01 +0700

    GameList

---
+++ GameListApp/Model/GameList.swift
@@ -0,0 +1,20 @@
+//
+//  GameList.swift
+//  GameListApp
+//
+//  Created by C-70 on 09/01/22.
+//
+
+import Foundation
+
+struct GameList: Codable {
+    var count: Int
+    var next: String
+    var results: [Game]
+    
+    enum CodingKeys: String, CodingKey {
+        case count
+        case next
+        case results
+    }
+}
